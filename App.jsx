58f65272ddf7f4983cb1b02e573b2075f3d890b6
Muhammad Mohsin Mukhtiar <139309941+MohsinMukhtiar@users.noreply.github.com>
2025-11-05 14:06:56 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -12,7 +12,7 @@ import About from "./src/components/About";
 import Contact from "./src/components/Cotact";
 import Admin from "./src/components/Admin";
 
-import "../Frontend/src/Index.css";
+import './src/index.css'; 
 
 export default function App() {
    function Home() {
